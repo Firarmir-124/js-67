86dff1ec108c667703b015f519f0e50a50a797a9
Polushin Dima <dima2004polushin2004@gmail.com>
2022-12-15 16:45:36 +0600

    Подключил компонент клавиатуру к компоненту калькулятор и реализовал ввод цифр и их сумму

--- src/containers/Calculator/Calculator.tsx
+++ src/containers/Calculator/Calculator.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import { useSelector} from "react-redux";
 import {RootState} from "../../app/store";
-import {Box, Button, ButtonGroup, Grid, Paper, Typography} from "@mui/material";
-import './Calculator.css';
-
-const NUMBERS = [
-  {number: 7, value: '7'},
-  {number: 8, value: '8'},
-  {number: 9, value: '9'},
-  {number: 4, value: '4'},
-  {number: 5, value: '5'},
-  {number: 6, value: '6'},
-  {number: 1, value: '1'},
-  {number: 2, value: '2'},
-  {number: 3, value: '3'},
-  {number: 0, value: '0'},
-]
+import {Box, Paper} from "@mui/material";
+import Keyboard from "../Keyboard/Keyboard";
 
 const Calculator = () => {
   const calculatorValue = useSelector((state:RootState) => state.calculator.value);
@@ -26,23 +13,7 @@ const Calculator = () => {
         <p>{calculatorValue}</p>
       </Paper>
       <Paper elevation={2} sx={{display: 'flex'}}>
-
-        <Box sx={{width: '200px'}}>
-          {NUMBERS.map(num => (
-            <Button sx={{width: '50px', height: '50px', mb: '10px'}} variant="outlined">{num.value}</Button>
-          ))}
-        </Box>
-
-
-        <ButtonGroup orientation="vertical" aria-label="vertical outlined button group">
-          <Button sx={{width: '50px', height: '50px'}} variant="outlined">-</Button>
-          <Button sx={{width: '50px', height: '50px'}} variant="outlined">+</Button>
-          <Button sx={{width: '50px', height: '50px'}} variant="outlined">*</Button>
-          <Button sx={{width: '50px', height: '50px'}} variant="outlined">/</Button>
-          <Button sx={{width: '50px', height: '50px'}} variant="outlined">=</Button>
-        </ButtonGroup>
-
-
+        <Keyboard/>
       </Paper>
     </Box>
   );
