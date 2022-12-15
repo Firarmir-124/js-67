5b2540e647e1ac41f0439115f6d3ec8e7c36fe34
Polushin Dima <dima2004polushin2004@gmail.com>
2022-12-15 16:42:25 +0600

    Создал компонент клавиатуры (кнопки калькулятора) Сделал верстку и событие кликика

---
+++ src/containers/Keyboard/Keyboard.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import {Box, Button, ButtonGroup, Paper} from "@mui/material";
+import {answer, inputValue} from "../Calculator/CalculatorSlice";
+import {useDispatch} from "react-redux";
+
+const NUMBERS = [
+  {value: '7'},
+  {value: '8'},
+  {value: '9'},
+  {value: '4'},
+  {value: '5'},
+  {value: '6'},
+  {value: '1'},
+  {value: '2'},
+  {value: '3'},
+  {value: '0'},
+]
+
+const Keyboard = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <Box sx={{width: '200px'}}>
+        {NUMBERS.map(num => (
+          <Button
+            onClick={() => dispatch(inputValue(num.value))}
+            sx={{width: '50px', height: '50px', mb: '10px'}}
+            variant="outlined"
+          >
+            {num.value}
+          </Button>
+        ))}
+      </Box>
+
+
+
+
+      <ButtonGroup orientation="vertical" aria-label="vertical outlined button group">
+        <Button
+          onClick={() => dispatch(inputValue('-'))}
+          sx={{width: '50px', height: '50px'}}
+          variant="outlined"
+        >
+          -
+        </Button>
+        <Button
+          onClick={() => dispatch(inputValue('+'))}
+          sx={{width: '50px', height: '50px'}}
+          variant="outlined"
+        >
+          +
+        </Button>
+        <Button
+          onClick={() => dispatch(inputValue('*'))}
+          sx={{width: '50px', height: '50px'}}
+          variant="outlined"
+        >
+          *
+        </Button>
+        <Button
+          onClick={() => dispatch(answer())}
+          sx={{width: '50px', height: '50px'}}
+          variant="outlined"
+        >
+          =
+        </Button>
+        <Button
+          onClick={() => dispatch(inputValue('/'))}
+          sx={{width: '50px', height: '50px'}}
+          variant="outlined"
+        >
+          /
+        </Button>
+      </ButtonGroup>
+    </>
+  );
+};
+
+export default Keyboard;
